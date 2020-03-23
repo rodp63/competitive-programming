51ae63d3928b5b29d2ff0fabeef88e0ea8dd6cb5
Rodp63 <joaquin.palma@ucsp.edu.pe>
2020-03-23 14:45:35 -0500

    Hash

--- hashingPal.cpp
+++ hashingPal.cpp
@@ -1,84 +1,53 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <math.h>
-#include <algorithm>
 #include <stdio.h>
-
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <deque>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <locale>
+#include <list>
+#include <bitset>
+#include <tuple>
 using namespace std;
-
-typedef unsigned long long ll;
-ll p=1;
-vector<ll> multi;
-
-void multiplicar(int n){
-    multi.push_back(1);
-    for(int i=1; i<=n; i++){
-        p=(p*31);
-        multi.push_back(p);
-    }
+#define FORN(i,m,n) for(int i=(m); i<int(n); i++)
+#define PRINTVEC(v) FORN(i,0,v.size()) cout<<v[i]<<" "; cout<<endl
+#define PRINTMAT(m) FORN(j,0,m.size()) {PRINTVEC(m[j]);}
+typedef long long ll;
+ 
+const ll mod = 1e9 + 7, ms = (1ll << 31) - 1;
+ 
+void Hash(string &s, vector<ll> &v, vector<ll> &m){
+  v[0] = s[0];
+  FORN(i,1,s.size()) v[i] = (v[i-1] + ((s[i] * m[i]) % mod)) % mod;
+}
+ 
+bool match(int a, int b, int c, int d, vector<ll> &h1, vector<ll> &m){
+  ll x, y;
+  int n = h1.size();
+  x = h1[b];
+  if(a) x = (x - h1[a-1] + mod) % mod;
+  y = h1[d];
+  if(c) y = (y - h1[c-1] + mod) % mod;
+  x = (x * m[n-a]) % mod;
+  y = (y * m[n-c]) % mod;
+  return x == y;
 }
 
-void hasH(string s, vector<ll> &v){
-    v[0]=s[0];
-    for(int i=1; i<s.length(); i++){
-        v[i]=v[i-1]+(s[i]*multi[i]);
-        //cout<<v[i]<<endl;
-    }
-}
-
-bool buscar(int a, int b, int c, int d, vector<ll> &v, vector<ll> &v1){
-
-    ll temp1,temp2;
-    int n=v.size();
-    if(a==0) temp1=v[b];
-    else temp1=v[b]-v[a-1];
-    if(c==0) temp2=v1[d];
-    else temp2=v1[d]-v1[c-1];
-
-    temp1=temp1*multi[n-a];
-    temp2=temp2*multi[n-c];
-
-    if(temp1==temp2) return true;
-    else return false;
-}
-
-int main()
-{
-    string s;
-    cin>>s;
-    multiplicar(s.length());
-    vector<ll> v1(s.length());
-    vector<ll> v2(s.length());
-    hasH(s,v1);
-    reverse(s.begin(),s.end());
-    hasH(s,v2);
-    vector<int> res(s.length(),0);
-
-    for(int i=0; i<s.length()-1; i++){
-        int d;
-        d=s.length()-i-1;
-        for(int j=i+1; j<s.length(); j++){
-            int c=s.length()-j-1;
-            if(buscar(i,j,c,d,v1,v2)==true) {
-                bool temp=true;
-                int cont=0;
-                int a=i;
-                int b=j;
-                while(temp==true){
-                    if(a==b){res[cont]++; break;}
-                    else{
-                        b=(a+b-1)/2;
-                        temp=buscar(a,b,s.length()-b-1,s.length()-a-1,v1,v2);
-                        res[cont]++;
-                    }
-                    cont++;
-                }
-            } //cout<<i<<" "<<j<<endl;}
-        }
-    }
-    res[0]+=s.length();
-    for(int i=0; i<res.size(); i++){
-        cout<<res[i]<<" ";
-    }
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  string s; cin>>s;
+  int a, b, c, d;
+  cin>>a>>b>>c>>d;
+  int n = s.size();
+  vector<ll> m(n+1), h1(n);
+  m[0] = 1;
+  FORN(i,1,n+1) m[i] = (m[i-1] * ms) % mod;
+  Hash(s, h1, m);
+  cout<<(match(a,b,c,d,h1,m) ? "YES" : "NO")<<endl;
 }

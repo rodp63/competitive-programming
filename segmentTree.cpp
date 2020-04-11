cf922c3b54abb5fa43343ec2f6c425cc291e3373
Rodp63 <joaquin.palma@ucsp.edu.pe>
2020-04-11 10:48:46 -0500

    SegmentTree

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -8,68 +8,48 @@
 #include <map>
 #include <deque>
 #include <math.h>
-#include <string.h>
+#include <string>
 #include <iomanip>
 #include <locale>
 #include <list>
+#include <bitset>
+#include <tuple>
 using namespace std;
-#define FORN(i,m,n) for(int i=m; i<(n); i++)
+#define FORN(i,m,n) for(int i=(m); i<int(n); i++)
 #define PRINTVEC(v) FORN(i,0,v.size()) cout<<v[i]<<" "; cout<<endl
 #define PRINTMAT(m) FORN(j,0,m.size()) {PRINTVEC(m[j]);}
-#define p_b(x) push_back(x)
-#define m_p(a,b) make_pair(a,b)
 typedef long long ll;
-typedef long double ld;
 
-const int N = 4e5; // MAX size
-const int null = -1; // min, 0 for sum
-int Tree[N];
-vector<int> numbers;
+const int ms = 1e5 + 5;
+int n, q;
+int tree[ms<<1];
 
-int f(int a, int b){
-  return max(a,b);
+void build(){
+  for(int i = n - 1; ~i; --i) tree[i] = max(tree[i<<1], tree[i<<1|1]);
 }
 
-void build(int v, int l, int r){
-  if(l == r) Tree[v] = numbers[l];
-  else{
-    int mid = (l + r) >> 1, next = (v << 1);
-    build(next, l, mid);
-    build(next+1, mid+1, r);
-    Tree[v] = f(Tree[next],Tree[next+1]);
+void update(int p, int value){
+  for(tree[p += n] = value; p > 1; p >>= 1) tree[p>>1] = max(tree[p], tree[p^1]);
+}
+
+int query(int l, int r){
+  int res = -1;
+  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    if (l&1) res = max(res, tree[l++]);
+    if (r&1) res = max(res, tree[--r]);
   }
-}
-
-int query(int v, int l, int r, int ql, int qr){
-  if(ql > qr) return null;
-  if(ql == l && qr == r) return Tree[v];
-  int mid = (l + r) >> 1, next = (v << 1), A, B;
-  A = query(next, l, mid, ql, min(qr, mid));
-  B = query(next+1, mid+1, r, max(ql, mid+1), qr);
-  return f(A, B);
-}
-
-void update(int v, int l, int r, int pos, int val){
-  if(l == r) Tree[v] = val;
-  else{
-    int mid = (l + r) >> 1, next = (v << 1);
-    if(pos <= mid) update(next, l, mid, pos, val);
-    else update(next+1, mid+1, r, pos, val);
-    Tree[v] = f(Tree[next], Tree[next+1]);
-  }
+  return res;
 }
 
 int main(){
   ios_base::sync_with_stdio(false); cin.tie(NULL);
-  int n, m, ans = 0;
-  cin>>n>>m;
-  numbers = vector<int> (n);
-  FORN(i,0,n) cin>>numbers[i];
-  build(1,0,n-1);
-  FORN(i,0,m){
-    int a, b; cin>>a>>b;
-    a--, b-=2;
-    if(query(1,0,n-1,a,b) <= numbers[a]) ans++;
+  cin>>n>>q;
+  FORN(i,0,n) cin>>tree[n+i];
+  build();
+  FORN(i,0,q){
+    int a, b;
+    cin>>a>>b;
+    a--;
+    cout<<query(a,b)<<endl; //query: [a,b)
   }
-  cout<<ans<<endl;
 }

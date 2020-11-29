7a32535a4a72bd9ba0e47f2f5f64a5ccc163a187
Rodp63 <joaquin.palma@ucsp.edu.pe>
2020-11-29 15:31:34 -0500

    Base code template

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <deque>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <locale>
+#include <list>
+#include <bitset>
+#include <tuple>
+#include <fstream>
+using namespace std;
+ 
+#define FORN(i,m,n) for(int i=(m); i<int(n); ++i)
+#define PRINTARR(a,n) FORN(i,0,n) cout<<a[i]<<" "; cout<<endl
+#define PRINTVEC(v) FORN(i,0,v.size()) cout<<v[i]<<" "; cout<<endl
+#define PRINTMAT(m) FORN(j,0,m.size()) {PRINTVEC(m[j]);}
+
+typedef long long ll;
+
+const int inf = 1e9 + 9;
+const int ms = 1e5 + 5;
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+}
